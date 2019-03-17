90e7dfe7a41c09c11b008f33190ee39ed72c8823
Jesyfox <jesyfoxbs@gmail.com>
2019-03-17 22:57:36 +0000

    feature: updated model

--- mytheresa/trophy_viewer/models.py
+++ mytheresa/trophy_viewer/models.py
@@ -1,10 +1,11 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
 
 class MytheresaItem(models.Model):
     article = models.CharField(max_length=100)
     title = models.CharField(max_length=100)
-    image = models.CharField(max_length=100)
+    image = ArrayField(models.CharField(max_length=100))
     price = models.CharField(max_length=100)
-    size = models.CharField(max_length=100)
-    description = models.CharField(max_length=100)
+    size = ArrayField(models.CharField(max_length=100))
+    description = models.TextField()
