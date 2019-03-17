604715be45e5d8f3a6d939969822d4613fad27dc
Jesyfox <jesyfoxbs@gmail.com>
2019-03-17 21:44:48 +0000

    feature: updated view, added raw model

--- mytheresa/trophy_viewer/models.py
+++ mytheresa/trophy_viewer/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class MytheresaItem(models.Model):
+    article = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    image = models.CharField(max_length=100)
+    price = models.CharField(max_length=100)
+    size = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
--- mytheresa/trophy_viewer/views.py
+++ mytheresa/trophy_viewer/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+
+from .models import MytheresaItem
 
 
-class Index(TemplateView):
-    template_name = "index.html"
+class Index(ListView):
+    template_name = "trophy_viewer/index.html"
+
+    model = MytheresaItem
+
+    def post(self, request, *args, **kwargs):
+        return self.get(self, request, *args, **kwargs)
